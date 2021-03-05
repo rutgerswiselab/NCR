73e4fc5d0eb7ad7431d0b19f97edbf857dc22694
Hanxiong Chen <41347387+TalonCB@users.noreply.github.com>
2021-03-05 17:37:46 -0500

    Add files via upload

--- src/utils/dataset.py
+++ src/utils/dataset.py
@@ -47,37 +47,47 @@ def random_split_data(all_data_file, dataset_name, vt_ratio=0.1, u_f=None, i_f=N
 
 def leave_out_by_time(all_data_file, dataset_name, leave_n=1, warm_n=5, u_f=None, i_f=None):
     """
-    默认all_data里的交互是按时间顺序排列的，按交互顺序，把最后的交互划分到验证集合测试集里
-    :param all_data_file: 数据预处理完的文件 *.all.csv，交互按时间顺序排列
-    :param dataset_name: 给数据集起个名字
-    :param leave_n: 验证和测试集保留几个用户交互
-    :param warm_n: 保证测试用户在训练集中至少有warm_n个交互，否则交互全部放在训练集中
-    :param u_f: 用户特征文件 *.user.csv
-    :param i_f: 物品特征文件 *.item.csv
-    :return: pandas dataframe 训练集,验证集,测试集
+    Split train/validation/test by timestamp.
+    By default, the interactions in all_data_file are already sorted by timestamp.
+    :param all_data_file: preprocessed dataset file *.all.csv，which is sorted by timestamp.
+    :param dataset_name: dataset name (used as the processed dataset name)
+    :param leave_n: number of items that are left in validation and test set.
+    :param warm_n: minimum number of interactions to leave in training dataset for each user.
+    :param u_f: user feature file (not used here)
+    :param i_f: item feature file (not used here)
+    :return: pandas dataframe for training/validation/test sets
     """
     dir_name = os.path.join(global_p.DATASET_DIR, dataset_name)
     print('leave_out_by_time', dir_name, leave_n, warm_n)
-    if not os.path.exists(dir_name):  # 如果数据集文件夹dataset_name不存在，则创建该文件夹，dataset_name是文件夹名字
+    if not os.path.exists(dir_name):
         os.mkdir(dir_name)
     all_data = pd.read_csv(all_data_file, sep='\t')
     min_label = all_data['label'].min()
 
-    if min_label > 0:  # 所有交互皆为正向交互
-        # 训练集保留每个用户的至少warm_n个正向交互，不够则全部保留，以保证验证集、测试集非冷启动
+    if min_label > 0:  # all the interactions are positive
+        '''
+        Keep at least 'warm_n' number of interactions in training dataset. 
+        If user has less than 'warm_n' interactions, then keep all the interactions in training set.
+        This is to guarantee that no cold start issue for validation and testing.
+        '''
         train_set = all_data.groupby('uid').head(warm_n)
         all_data = all_data.drop(train_set.index)
 
-        # 先满足test_set取倒数leave_n个
+        # Get last leave_n items to put into test_set
         test_set = all_data.groupby('uid').tail(leave_n)
         all_data = all_data.drop(test_set.index)
-        # 再满足validation_set取剩下的倒数leave_n个
+        # Get validation_set from the remaining last leave_n items
         validation_set = all_data.groupby('uid').tail(leave_n)
         all_data = all_data.drop(validation_set.index)
-    else:  # 数据集中存在负向交互
-        # 训练集保留每个用户的至少warm_n个正向交互，不够则全部保留，以保证验证集、测试集非冷启动
+    else:  # there are negative interactions
+        '''
+        Keep at least 'warm_n' number of interactions in training dataset. 
+        If user has less than 'warm_n' interactions, then keep all the interactions in training set.
+        This is to guarantee that no cold start issue for validation and testing.
+        '''
         train_set = []
-        for uid, group in all_data.groupby('uid'):
+        # generate training set by looking for the first warm_n POSITIVE interactions
+        for uid, group in all_data.groupby('uid'):  # group by uid
             found, found_idx = 0, -1
             for idx in group.index:
                 if group.loc[idx, 'label'] > 0:
@@ -88,44 +98,47 @@ def leave_out_by_time(all_data_file, dataset_name, leave_n=1, warm_n=5, u_f=None
             if found_idx > 0:
                 train_set.append(group.loc[:found_idx + 1])
         train_set = pd.concat(train_set)
+        # drop the training data info
         all_data = all_data.drop(train_set.index)
 
+        # generate test set by looking for the last leave_n POSITIVE interactions
         test_set = []
-        for uid, group in all_data.groupby('uid'):  # 按uid聚合
+        for uid, group in all_data.groupby('uid'):
             found, found_idx = 0, -1
-            for idx in reversed(group.index):  # 倒着看该用户的历史，直到找到一个正样本
+            for idx in reversed(group.index):
                 if group.loc[idx, 'label'] > 0:
                     found_idx = idx
                     found += 1
                     if found >= leave_n:
                         break
-            if found_idx > 0:  # 如果找到了正样本，则该样本及其之后的负样本全部放入测试集
+            if found_idx > 0:
                 test_set.append(group.loc[found_idx:])
         test_set = pd.concat(test_set)
         all_data = all_data.drop(test_set.index)
 
         validation_set = []
-        for uid, group in all_data.groupby('uid'):  # 按uid聚合
+        for uid, group in all_data.groupby('uid'):  #
             found, found_idx = 0, -1
-            for idx in reversed(group.index):  # 倒着看该用户的历史，直到找到一个正样本
+            for idx in reversed(group.index):
                 if group.loc[idx, 'label'] > 0:
                     found_idx = idx
                     found += 1
                     if found >= leave_n:
                         break
-            if found_idx > 0:  # 如果找到了正样本，则该样本及其之后的负样本全部放入验证集
+            # put all the negative interactions encountered during the search process into validation set
+            if found_idx > 0:
                 validation_set.append(group.loc[found_idx:])
         validation_set = pd.concat(validation_set)
         all_data = all_data.drop(validation_set.index)
 
-    # 剩下的样本再放入训练集
+    # The remaining data (after removing validation and test) are all in training data
     train_set = pd.concat([train_set, all_data]).sort_index()
     validation_set, test_set = validation_set.sort_index(), test_set.sort_index()
 
     train_set.to_csv(os.path.join(dir_name, dataset_name + '.train.csv'), index=False, sep='\t')
     validation_set.to_csv(os.path.join(dir_name, dataset_name + '.validation.csv'), index=False, sep='\t')
     test_set.to_csv(os.path.join(dir_name, dataset_name + '.test.csv'), index=False, sep='\t')
-    # 复制用户、物品的特征文件
+    # Copy feature info if applicable
     if u_f is not None:
         copyfile(u_f, os.path.join(dir_name, dataset_name + '.user.csv'))
     if i_f is not None:
